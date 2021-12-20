4211cdf73c65e3e62b196d1e101857e7869c1f73
aimad bachar <aimadbacharpro@outlook.fr>
2021-12-20 14:59:27 +0100

    dossier

--- ExoChrono/main.js
+++ ExoChrono/main.js
@@ -8,13 +8,18 @@ let sec = 0; // secondes
 let min = 0; // minutes
 let hrs = 0; // heures
 let timerOn = false;
+
+
+
+
 /******************** FONCTIONS ******************** */
 
 function main() {
-
     // Récupération des éléments du DOM
     let btnStart = document.querySelector("#startStop");
     let btnReset = document.querySelector("#reset");
+    btnStart.innerText = "Start";
+
 
     // Zone du DOM qui contient le compteur
     let timerDom = document.querySelector('#chrono');
@@ -24,11 +29,13 @@ function main() {
         console.log(timerOn);
         if (timerOn === false) {
             timerOn = true;
-            timer = updateTimer(timerDom);
+            timer = updateTimer();
+            btnStart.innerText = "Stop";
             // Execution d'une fonction de mise à jour du compteur toutes les secondes. Cette fonction reçoit en paramètre la zone du DOM à mettre à jour
         } else {
             timerOn = false;
             clearTimeout(timer);
+            btnStart.innerText = "Start";
         }
 
     });
@@ -37,24 +44,26 @@ function main() {
     // Evènement qui détecte quand on clique sur le bouton "reset"
     btnReset.addEventListener('click', function () {    
         clearTimeout(timer);
-        timerDom.innerHTML = "00h:00m:00s:00";
+        timerDom.innerText = "00h:00m:00s:00";
         sec = 0;
         min = 0;
         hrs = 0;
         timerOn = false;
+        btnStart.innerText = "Start";
     });
 
+    
     /** Fonction qui met à jour le timer en ajoutant 1
     * @param {HTMLElement} objet du DOM où est contenu le texte du Timer
     */
-    function updateTimer(timerDom) {
+    function updateTimer() {
         // on met à jour la zone HTML
         if (centSec === 99) {
             centSec = 0;
-            sec++;
+            ++sec;
             if (sec > 59) {
                 sec = 0;
-                min++;
+                ++min;
                 if (min >= 60) {
                     min = 0;
                     hrs++;
@@ -64,13 +73,12 @@ function main() {
             
 
             
-        timerDom.innerHTML = (hrs > 9 ? hrs : "0" + hrs) 
+        timerDom.innerText = (hrs > 9 ? hrs : "0" + hrs) 
         + ":" + (min > 9 ? min : "0" + min)
            + ":" + (sec > 9 ? sec : "0" + sec)
-           + ":" + (++centSec > 9 ? centSec : "0" + centSec);
-        timer = window.setTimeout(updateTimer, 10, timerDom)
+           + ":" + (centSec++ > 9 ? centSec : "0" + centSec);
+        timer = window.setTimeout(updateTimer, 10)
     }
-
 };
 
 
