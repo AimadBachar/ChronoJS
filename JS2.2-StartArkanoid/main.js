9e8ec84b3cea1773fa7880eb054b65155f911d3d
aimad bachar <aimadbacharpro@outlook.fr>
2021-12-21 12:38:15 +0100

    add start/stop animation when click on screen

--- JS2.2-StartArkanoid/main.js
+++ JS2.2-StartArkanoid/main.js
@@ -1,64 +1,82 @@
-'use strict'
+"use strict";
 
 /***********************************************************************************/
 /* *********************************** DONNEES *************************************/
 /***********************************************************************************/
 let ball_DOM;
 let ball = {
-    positionX: 0,
-    positionY: 0,
-}
-let directionX = 10;
-let directionY = 10;
-let windowWidth;
-let windowHeight;
-
-
+  positionX: 0,
+  positionY: 0,
+  directionX: 1,
+  directionY: 1,
+};
+let windowScreen = {
+  windowWidth: 0,
+  windowHeight: 0,
+};
+let animation_id;
+let animationPlay = true;
 
 /***********************************************************************************/
 /* ********************************** FONCTIONS ************************************/
 /***********************************************************************************/
 function moveBall() {
-    // Récupération de la taille de la fenêtre
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-    // Changement de la position de la balle en fonction de la direction
-    ball.positionX += directionX;
-    ball_DOM.style.left = `${ball.positionX}px`;
-    ball.positionY += directionY;
-    ball_DOM.style.top = `${ball.positionY}px`;
+  animationPlay = true;
+  // Récupération de la taille de la fenêtre
+  windowScreen.windowWidth = window.innerWidth;
+  windowScreen.windowHeight = window.innerHeight;
+  // Changement de la position de la balle en fonction de la direction
+  ball.positionX += ball.directionX * 10;
+  ball_DOM.style.left = `${ball.positionX}px`;
+  ball.positionY += ball.directionY * 10;
+  ball_DOM.style.top = `${ball.positionY}px`;
 
-    // Changement de la direction de la balle
-    if (ball.positionX >= windowWidth - ball_DOM.clientWidth || ball.positionX <= 0) {   // Si la balle sort de la fenêtre , ball_DOM.clientWidth = largeur de la balle
-        directionX *= -1;
-    }
+  // Changement de la direction de la balle
+  if (
+    ball.positionX >= windowScreen.windowWidth - ball_DOM.clientWidth ||
+    ball.positionX <= 0
+  ) {
+    // Si la balle sort de la fenêtre , ball_DOM.clientWidth = largeur de la balle
+    ball.directionX *= -1;
+  }
 
-    if (ball.positionY >= windowHeight - ball_DOM.clientHeight || ball.positionY <= 0) {
-        directionY *= -1;
-    }
+  if (
+    ball.positionY >= windowScreen.windowHeight - ball_DOM.clientHeight ||
+    ball.positionY <= 0
+  ) {
+    ball.directionY *= -1;
+  }
 
-    // Réappel de la fonction
-    window.requestAnimationFrame(moveBall);
+  // Réappel de la fonction
+  animation_id = window.requestAnimationFrame(moveBall);
 }
 
-
+function stopMoveBall() {
+  window.cancelAnimationFrame(animation_id);
+  animationPlay = false;
+}
 
 /************************************************************************************/
 /* ******************************** CODE PRINCIPAL **********************************/
 /************************************************************************************/
 function main() {
-    // Récupération des éléments du DOM
-    ball_DOM = document.querySelector("#ball");
+  // Récupération des éléments du DOM
+  ball_DOM = document.querySelector("#ball");
 
+  // Animation
+  animation_id = window.requestAnimationFrame(moveBall);
 
-    // Animation
-    window.requestAnimationFrame(moveBall);
-    
+  // Gestion des évènements
+  document.addEventListener("click", () => {
+    if (animationPlay === true) {
+      // Au clic sur l'écran stop la balle
+      stopMoveBall();
+    } else {
+      // Au clic sur l'écran relance la balle
+      moveBall();
+    }
+  });
 }
 
-
-
-
-
 /********************* MAIN  ***************************/
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
